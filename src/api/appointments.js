e6b9e9f5941656d5093040f65a959cbba52c9e79
anas el assri <anas.elassri.01@gmail.com>
2024-12-16 11:39:47 +0100

    fing Api

--- src/api/appointments.js
+++ src/api/appointments.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://barbershop-back-end.vercel.app/api";
 
 export const fetchAppointments = async () => {
   try {
